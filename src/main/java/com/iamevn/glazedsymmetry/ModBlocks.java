ef1162509bbded91ce427d1d046e166a8c92bc91
iamevn <iamevn@users.noreply.github.com>
2020-11-14 17:26:44 -0800

    orange blocks work, inventory doesn't
    fix inventory icons and then regenerate other colors' files

--- src/main/java/com/iamevn/glazedsymmetry/ModBlocks.java
+++ src/main/java/com/iamevn/glazedsymmetry/ModBlocks.java
@@ -1,7 +1,9 @@
 package com.iamevn.glazedsymmetry;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -9,7 +11,25 @@ import net.minecraftforge.fml.RegistryObject;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    static void register() {}
+    final private static String[] COLORS = {"orange"};
+
+    private static DyeColor getColor(String color) {
+        switch (color) {
+            case "orange":
+                return DyeColor.ORANGE;
+        }
+        return DyeColor.WHITE;
+    }
+    private static AbstractBlock.Properties GlazedTerracottaProperties(String color) {
+        return AbstractBlock.Properties.create(Material.ROCK, getColor(color)).setRequiresTool().hardnessAndResistance(1.4F);
+    }
+
+    static void register() {
+        for (String color: COLORS) {
+            register(color + "_centered_glazed_terracotta", () -> new GlazedTerracottaBlock(GlazedTerracottaProperties(color)));
+            register(color + "_glazed_terracotta_pillar", () -> new RotatedPillarBlock(GlazedTerracottaProperties(color)));
+        }
+    }
 
     private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block) {
         RegistryObject<T> reg_block = Registration.BLOCKS.register(name, block);
