50de21b03603d3adbd041366968e9e4da3680342
iamevn <iamevn@users.noreply.github.com>
2020-11-14 19:15:26 -0800

    Add other colors

--- src/main/java/com/iamevn/glazedsymmetry/ModBlocks.java
+++ src/main/java/com/iamevn/glazedsymmetry/ModBlocks.java
@@ -11,12 +11,47 @@ import net.minecraftforge.fml.RegistryObject;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    final private static String[] COLORS = {"orange"};
+    final private static String[] COLORS = {
+            "white", "light_gray", "red", "purple",
+            "pink", "orange", "magenta", "lime",
+            "light_blue", "green", "gray", "cyan",
+            "brown", "blue", "yellow", "black"
+    };
 
     private static DyeColor getColor(String color) {
         switch (color) {
+            case "white":
+                return DyeColor.WHITE;
+            case "light_gray":
+                return DyeColor.LIGHT_GRAY;
+            case "red":
+                return DyeColor.RED;
+            case "purple":
+                return DyeColor.PURPLE;
+            case "pink":
+                return DyeColor.PINK;
             case "orange":
                 return DyeColor.ORANGE;
+            case "magenta":
+                return DyeColor.MAGENTA;
+            case "lime":
+                return DyeColor.LIME;
+            case "light_blue":
+                return DyeColor.LIGHT_BLUE;
+            case "green":
+                return DyeColor.GREEN;
+            case "gray":
+                return DyeColor.GRAY;
+            case "cyan":
+                return DyeColor.CYAN;
+            case "brown":
+                return DyeColor.BROWN;
+            case "blue":
+                return DyeColor.BLUE;
+            case "yellow":
+                return DyeColor.YELLOW;
+            case "black":
+                return DyeColor.BLACK;
         }
         return DyeColor.WHITE;
     }
